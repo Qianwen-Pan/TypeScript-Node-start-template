73bfe322b4a0e1870cf9e1657cb7dc5a1e763833
qianwen_pan <qianwen.pan2021@gmail.com>
2023-07-01 17:16:41 -0400

    add environment argument

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
+require("dotenv").config();
+
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from "body-parser"
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+//database setting
+const db_name = process.env.DB_NAME;
+const db_password = process.env.DB_PASSWORD;
+
+const url = `mongodb+srv://${db_name}:${db_password}@cluster0.iyauopq.mongodb.net/secrectsDB`;
+mongoose
+  .connect(url)
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err: any) => console.log(err));
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
